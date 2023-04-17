69366ff82248a2fd16120f17801a8fc2b1cbb73e
chiaoa <130937187+chiaoa@users.noreply.github.com>
2023-04-17 12:23:48 -0400

    Update algorithms.py

--- algorithms.py
+++ algorithms.py
@@ -70,62 +70,84 @@ def BFS(initial_state, goal_state):
                             queue.put((child_state, path + ["R"], child_cost))
     return -1
 
-
-def isEmpty(self):
-        return self.size() == 0
-
     
 def DFS(initial_state, goal_state):
+    # initialize stack for dfs traversal
     stack = [(initial_state, [], 0)]
+    # initialize set of discovered states and add initial state
     visited = set()
+    visited.add(tuple(map(tuple, initial_state)))
 
+    # while the stack is not empty (still got states to look at)
     while stack:
+        # extract and pop "newest" state (lifo) --> current state
         state, path, cost = stack.pop()
+        
+        # if the state is the goal state, yay! end this ish
         if state == goal_state:
             return state, path, cost
-        visited.add(tuple(map(tuple, state)))
-
+        
+        """ check the position of the empty tile in the matrix
+            and generate child states (next possible states) by moving the empty tile """
+        # for each row i and column j in the puzzle matrix...
         for i in range(0, 3):
             for j in range(0, 3):
+                # once row i and column j is the location of the empty tile, append all next possible states
                 if state[i][j] == 0:
                     if j < 2:  # tile can move right
-                        child_state = tuple([tuple(row) for row in state])
-                        child_state = list(map(list, child_state))
+                        # set child state as current state
+                        child_state = list(map(list, state))
+                        # swap empty tile with tile to the right of it
                         child_state[i][j], child_state[i][j+1] = child_state[i][j+1], child_state[i][j]
+                        # increment the cost
                         child_cost = cost + 1
+                        # if the new state hasn't been visited, append it and mark it discovered
                         if tuple(map(tuple, child_state)) not in visited:
                             stack.append((child_state, path + ["R"], child_cost))
                             visited.add(tuple(map(tuple, child_state)))
+                            # if the new state is goal state, end search early
                             if child_state == goal_state:
                                 return child_state, path + ["R"], child_cost
                     if j > 0:  # tile can move left
-                        child_state = tuple([tuple(row) for row in state])
-                        child_state = list(map(list, child_state))
+                        # set child state as current state
+                        child_state = list(map(list, state))
+                        # swap empty tile with tile to the left of it
                         child_state[i][j], child_state[i][j-1] = child_state[i][j-1], child_state[i][j]
+                        # increment the cost
                         child_cost = cost + 1
+                        # if the new state hasn't been visited, append it and mark it discovered
                         if tuple(map(tuple, child_state)) not in visited:
                             stack.append((child_state, path + ["L"], child_cost))
                             visited.add(tuple(map(tuple, child_state)))
+                            # if the new state is goal state, end search early
                             if child_state == goal_state:
                                 return child_state, path + ["L"], child_cost
                     if i < 2:  # tile can move down
-                        child_state = tuple([tuple(row) for row in state])
-                        child_state = list(map(list, child_state))
+                        # set child state as current state
+                        child_state = list(map(list, state))
+                        # swap empty tile with tile below it
                         child_state[i][j], child_state[i+1][j] = child_state[i+1][j], child_state[i][j]
+                        # increment the cost
                         child_cost = cost + 1
+                        # if the new state hasn't been visited, append it and mark it discovered
                         if tuple(map(tuple, child_state)) not in visited:
                             stack.append((child_state, path + ["D"], child_cost))
                             visited.add(tuple(map(tuple, child_state)))
+                            # if the new state is goal state, end search early
                             if child_state == goal_state:
                                 return child_state, path + ["D"], child_cost
                     if i > 0:  # tile can move up
-                        child_state = tuple([tuple(row) for row in state])
-                        child_state = list(map(list, child_state))
+                        # set child state as current state
+                        child_state = list(map(list, state))
+                        # swap empty tile with tile above it
                         child_state[i][j], child_state[i-1][j] = child_state[i-1][j], child_state[i][j]
+                        # increment the cost
                         child_cost = cost + 1
+                        # if the new state hasn't been visited, append it and mark it discovered
                         if tuple(map(tuple, child_state)) not in visited:
                             stack.append((child_state, path + ["U"], child_cost))
                             visited.add(tuple(map(tuple, child_state)))
+                            # if the new state is goal state, end search early
                             if child_state == goal_state:
                                 return child_state, path + ["U"], child_cost
     return -1
